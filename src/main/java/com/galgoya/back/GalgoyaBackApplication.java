59001ac04c792822b3d6385e3519febf9ab05de4
opgumo <opgumo@naver.com>
2024-06-16 21:48:12 +0900

    init

---
+++ src/main/java/com/galgoya/back/GalgoyaBackApplication.java
@@ -0,0 +1,13 @@
+package com.galgoya.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalgoyaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalgoyaBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/galgoya/back/GalgoyaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galgoya.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalgoyaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
